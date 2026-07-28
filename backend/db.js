741cf46a54251064fffd23e58f97bbf7b6a52329
mamme234 <ghazimuhammadilyas@gmail.com>
2026-07-28 12:07:13 +0300

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/alpha', {
+        const conn = await mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/alpha-super', {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
